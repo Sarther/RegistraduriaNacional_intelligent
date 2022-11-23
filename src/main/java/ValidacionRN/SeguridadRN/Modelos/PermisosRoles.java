0765ce2460f655740c7de7e694c74882c344c6e6
Martin Luna <mhluna6519@gmail.com>
2022-11-23 15:04:24 -0500

    Codigo permisos roles

--- src/main/java/ValidacionRN/SeguridadRN/Modelos/PermisosRoles.java
+++ src/main/java/ValidacionRN/SeguridadRN/Modelos/PermisosRoles.java
@@ -7,7 +7,6 @@ import org.springframework.data.mongodb.core.mapping.Document;
 
 @Data
 @Document
-
 public class PermisosRoles {
     @Id
     private String _id;
--- src/main/java/ValidacionRN/SeguridadRN/SeguridadRnApplication.java
+++ src/main/java/ValidacionRN/SeguridadRN/SeguridadRnApplication.java
@@ -9,5 +9,4 @@ public class SeguridadRnApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SeguridadRnApplication.class, args);
 	}
-
 }
